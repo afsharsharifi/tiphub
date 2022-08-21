4c4737907b471783bd222d0d2bf56c3b312d1b6b
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-08-21 18:40:24 +0430

    Teacher Model Created

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
-from .models import CustomUser
+from .models import CustomUser, Teacher
 
 
 class CustomUserAdmin(BaseUserAdmin):
@@ -10,13 +10,13 @@ class CustomUserAdmin(BaseUserAdmin):
     list_filter = ('is_admin', 'is_active')
     fieldsets = (
         (None, {'fields': ('phone', 'password')}),
-        ('اطلاعات شخصی', {'fields': ('fullname', 'email', 'image', 'biography')}),
+        ('اطلاعات شخصی', {'fields': ('fullname', 'email', 'image')}),
         ('دسترسی ها', {'fields': ('is_admin', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('fullname', 'phone', 'email', 'image', 'biography', 'password1', 'password2'),
+            'fields': ('fullname', 'phone', 'email', 'image', 'password1', 'password2'),
         }),
     )
     search_fields = ('fullname', 'phone', 'email')
@@ -24,5 +24,17 @@ class CustomUserAdmin(BaseUserAdmin):
     filter_horizontal = ()
 
 
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'username')
+    fieldsets = (
+        (None, {'fields': ('user', 'username')}),
+        ('اطلاعات شخصی', {'fields': ('title', 'about')}),
+        ('شبکه های اجتماعی', {'fields': ('instagram', 'twitter', 'github', 'gitlab', 'linkedin', 'telegram', 'email', 'website')}),
+        ('فایل رزومه', {'fields': ('resume',)}),
+    )
+    search_fields = ('user', 'username', 'email')
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Teacher, TeacherAdmin)
 admin.site.unregister(Group)
--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 import os
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-from django.utils import timezone
 
 from .managers import CustomUserManager
 
@@ -14,7 +13,13 @@ def get_extension_file(filename):
     return os.path.splitext(filename)[-1]
 
 
-def create_image_path(instance, filename):
+def create_image_path_user(instance, filename):
+    extension = get_extension_file(filename)
+    username = username_from_email(instance.email)
+    return f"users/{username}{extension}"
+
+
+def create_resume_path_teacher(instance, filename):
     extension = get_extension_file(filename)
     username = username_from_email(instance.email)
     return f"teachers/{username}{extension}"
@@ -24,8 +29,7 @@ class CustomUser(AbstractBaseUser):
     fullname = models.CharField(max_length=150, verbose_name="نام و نام خانوادگی")
     phone = models.CharField(max_length=11, unique=True, verbose_name="شماره تلفن")
     email = models.EmailField(max_length=255, unique=True, verbose_name="ایمیل")
-    biography = models.TextField(null=True, blank=True, verbose_name="بیوگرافی")
-    image = models.ImageField(upload_to=create_image_path, null=True, blank=True, verbose_name="تصویر پروفایل")
+    image = models.ImageField(upload_to=create_image_path_user, null=True, blank=True, verbose_name="تصویر پروفایل")
     joined = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ عضویت")
     is_active = models.BooleanField(default=True, verbose_name="فعال")
     is_admin = models.BooleanField(default=False, verbose_name="مدیر")
@@ -36,8 +40,8 @@ class CustomUser(AbstractBaseUser):
     REQUIRED_FIELDS = ['fullname', 'email']
 
     class Meta:
-        verbose_name = 'حساب'
-        verbose_name_plural = 'حساب ها'
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
 
     def __str__(self):
         return self.fullname
@@ -51,3 +55,26 @@ class CustomUser(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="کاربر")
+    username = models.CharField(max_length=250, unique=True, verbose_name="نام کاربری")
+    title = models.CharField(max_length=250, null=True, blank=True, verbose_name="عنوان")
+    about = models.TextField(null=True, blank=True, verbose_name="درباره")
+    instagram = models.CharField(max_length=250, null=True, blank=True, verbose_name="اینستاگرام")
+    twitter = models.CharField(max_length=250, null=True, blank=True, verbose_name="توییتر")
+    github = models.CharField(max_length=250, null=True, blank=True, verbose_name="گیت هاب")
+    gitlab = models.CharField(max_length=250, null=True, blank=True, verbose_name="گیت لب")
+    linkedin = models.CharField(max_length=250, null=True, blank=True, verbose_name="لینکدین")
+    telegram = models.CharField(max_length=250, null=True, blank=True, verbose_name="تلگرام")
+    email = models.EmailField(max_length=255, unique=True, null=True, blank=True, verbose_name="ایمیل (پروفایل)")
+    website = models.URLField(null=True, blank=True, verbose_name="وبسایت")
+    resume = models.FileField(upload_to=create_resume_path_teacher, null=True, blank=True, verbose_name="فایل رزومه")
+
+    class Meta:
+        verbose_name = 'استاد'
+        verbose_name_plural = 'اساتید'
+
+    def __str__(self):
+        return f"{self.user}"
