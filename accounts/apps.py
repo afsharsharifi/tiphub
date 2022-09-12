832d4eb4c4a76aa75f147a114dfd52db25fb8ce8
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-09-12 17:59:18 +0430

    Customizing django admin panel

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,7 +1,17 @@
 from django.apps import AppConfig
+from allauth.account.apps import AccountConfig
+from allauth.socialaccount.apps import SocialAccountConfig
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     verbose_name = "حساب های کاربران"
+
+
+class AccountConfig(AccountConfig):
+    AccountConfig.verbose_name = "حساب ها"
+
+
+class SocialAccountConfig(SocialAccountConfig):
+    SocialAccountConfig.verbose_name = "حساب ها در شبکه های اجتماعی"
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = 'پنل ادمین'
+admin.site.index_title = 'تیپ هاب'
+admin.site.site_title = 'مدیریت'
